3ed01001ff6a9b0034ee5d9b87b01755d29fd3d0
Samuel Alves <samuel241296@hotmail.com>
2020-01-19 19:44:38 -0300

    <Portfolio> terminado <PortfolioCard> criado

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button/';
+import GitHub from '@material-ui/icons/GitHub';
 import blueGrey from '@material-ui/core/colors/blueGrey';
 import grey from '@material-ui/core/colors/grey';
+
+import PortfolioCard from './PortfolioCard/PortfolioCard';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -34,51 +38,134 @@ const useStyles = makeStyles(theme => ({
     text: {
         padding: theme.spacing(3),
         textAlign: "justify",
+
     },
     subtitle: {
         marginTop: theme.spacing(1),
         fontWeight: 'bold',
         letterSpacing: '-1px',
+    },
+    git: {
+        display: 'flex',
+        flex: 1,
+    },
+    a: {
+        margin: 'auto',
+    },
+    icon: {
+        color: '#000',
+        width: theme.spacing(10),
+        height: theme.spacing(3),
     }
 }));
 
-const Portifolio = props => {
+const Portfolio = props => {
     const classes = useStyles();
 
+
+
+    //https://shielded-spire-68979.herokuapp.com/index.jsp
+    //https://frozen-ridge-71151.herokuapp.com/
+    //http://fierce-island-38673.herokuapp.com/
+    //https://pokedex-react-samuel2412.netlify.com/
+    //https://react-myburger-c854a.firebaseapp.com/
+    //https://react-gamedb.firebaseapp.com/
     return (
         <div className={classes.root}>
             <div className={classes.content}>
+
                 <Typography className={classes.title} variant='subtitle1' gutterBottom>
                     Portifólio
                     </Typography>
             </div>
             <div className={classes.content}>
                 <div className={classes.text}>
-                    <Typography className={classes.subtitle} variant="h5" component='h5'>
-                        Lorem ipsum
-                        </Typography>
-                    <Typography variant="overline" component='p' gutterBottom>
-                        Senior Software Engineer (Team Lead) •  Feb 2017 - Jun 2018
-                        </Typography>
-                    <Typography variant="body1" component='p' gutterBottom>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in cursus dolor. Vestibulum enim quam, egestas in ex ut, consequat ornare elit. Proin finibus augue id aliquam aliquet. Nunc luctus libero sed dolor dictum, dictum lobortis lorem feugiat. Nulla pretium sagittis accumsan. Morbi pellentesque magna massa. Pellentesque eu ex urna.
-                    </Typography>
 
-                    <Divider variant="middle" />
 
-                    <Typography className={classes.subtitle} variant="h5" component='h5'>
-                        Lorem ipsum
+                    <PortfolioCard title='GameDB' src='/gamedb.jpg' date='jan de 2020'>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Game DataBase, permite ao usuário pesquisar por jogos,
+                            ver seus detalhes e marca-los como favorito ou concluído.
+                            Também é possível visualizar a lista gerada pelo usuário em sua pagina de perfil.
                         </Typography>
-                    <Typography variant="overline" component='p' gutterBottom>
-                        Senior Software Engineer (Team Lead) •  Feb 2017 - Jun 2018
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Os dados dos jogos são consumidos através de
+                            requisições HTTP a API <a href='https://rawg.io/apidocs'>rawg.</a>
                         </Typography>
-                    <Typography variant="body1" component='p' gutterBottom>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in cursus dolor. Vestibulum enim quam, egestas in ex ut, consequat ornare elit. Proin finibus augue id aliquam aliquet. Nunc luctus libero sed dolor dictum, dictum lobortis lorem feugiat. Nulla pretium sagittis accumsan. Morbi pellentesque magna massa. Pellentesque eu ex urna.
-                    </Typography>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Aplicação desenvolvida com ReactJS e Material-UI utilizando bibliotecas como Redux, React-Redux, Axios e armazenando os dados no Firebase.
+                        </Typography>
+                        <a className={classes.git} href='https://github.com/samuel2412/react-gamedb'>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+
+                    <PortfolioCard title='Burger Builder' src='/burgerBuilder.jpg' date='dez de 2019 – jan de 2020'>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Aplicação que permite ao usuário selecionar os ingredientes que ele deseja em seu hambúrguer e realizar o pedido.
+                        </Typography>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Desenvolvida com ReactJS utilizando bibliotecas como Redux, React-Redux, Axios e armazenando os dados no Firebase.
+                        </Typography>
+                        <a className={classes.git} href='https://github.com/samuel2412/react-burger-app'>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+
+                    <PortfolioCard title='Pokedex' src='/pokedex.jpg' date='set de 2019 – dez de 2019'>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Aplicação desenvolvida com ReactJS que exibe uma lista de pokemons, permitindo ao usuário
+                            pesquisar e visualizar os detalhes do pokemon desejado.
+                            </Typography>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Os dados dos pokemons são consumidos através de
+                            requisições HTTP a API <a href='https://pokeapi.co/'>pokeapi.</a>
+                        </Typography>
+                        <a className={classes.git} href='https://github.com/samuel2412/pokedex-react'>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+
+
+                    <PortfolioCard title='Site de e-commerce' src='/ecommerce.jpg' date='set de 2019'>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Projeto construído usando SpringMVC, Hibernate, Java, JPA, JSP, Bootstrap, etc.
+                            </Typography>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Trata-se de uma aplicação que simula um e-commerce.
+                        </Typography>
+                        <a className={classes.git} href='https://github.com/samuel2412/ecommerce'>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+
+
+                    <PortfolioCard title='Financiamento de Experimentos' src='/experimento.jpg' date='set de 2019'>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Projeto construído usando SpringMVC, Hibernate, Java, JPA, JSP, Bootstrap, etc.
+                            </Typography>
+                        <Typography variant="body1" component='p' gutterBottom>
+                            Trata-se de uma aplicação que permite cadastros de Experimentos e que o usuário financie aqueles que o interesse.
+                        </Typography>
+                        <a className={classes.git} href='https://github.com/samuel2412/experimento'>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+
 
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
-export default Portifolio;
+export default Portfolio;
---
+++ src/components/Portfolio/PortfolioCard/PortfolioCard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import CardMedia from '@material-ui/core/CardMedia';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'auto',
+        borderRadius: '5px',
+        marginBottom: theme.spacing(5),
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    text: {
+        padding: theme.spacing(3),
+        textAlign: "justify",
+    },
+    subtitle: {
+        fontWeight: 'bold',
+        letterSpacing: '-1px',
+        textAlign: 'left'
+    },
+    preview: {
+        borderRadius: '4px',
+        height: '40%'
+    },
+
+    
+}));
+
+const PortfolioCard = props => {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.root}>
+            <div className={classes.content}>
+                <CardMedia
+                    className={classes.preview}
+                    component="img"
+                    alt={props.title}
+                    image={props.src}
+                    title={props.title}
+                />
+
+                <div className={classes.text}>
+                    <Typography className={classes.subtitle} variant="h5" component='h5'>
+                        {props.title}
+                        </Typography>
+                    <Typography variant="overline" component='p' gutterBottom>
+                       {props.date}
+                        </Typography>
+                    
+                        {props.children}
+                   
+                </div>
+            </div>
+        </Paper>
+    );
+}
+
+export default PortfolioCard;
