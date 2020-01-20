1a7e1819c6206d0005e4a579b134c20c771a3b56
Samuel Alves <samuel241296@hotmail.com>
2020-01-20 11:18:53 -0300

    dados do <Portfolio> sendo carregados do firebase

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Typography from '@material-ui/core/Typography';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button/';
 import GitHub from '@material-ui/icons/GitHub';
@@ -8,6 +10,7 @@ import blueGrey from '@material-ui/core/colors/blueGrey';
 import grey from '@material-ui/core/colors/grey';
 
 import PortfolioCard from './PortfolioCard/PortfolioCard';
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -60,7 +63,62 @@ const useStyles = makeStyles(theme => ({
 
 const Portfolio = props => {
     const classes = useStyles();
+    const [isLoading, setIsLoading] = useState(true);
+    const [projetos, setProjetos] = useState([]);
 
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get('https://react-resume-samuel.firebaseio.com/projetos.json')
+            .then(res => {
+                //console.log(res.data);
+                const aux = [];
+                for (let key in res.data) {
+                    const desc = [];
+                    for (let innerKey in res.data[key].descricao) {
+                        desc.push(res.data[key].descricao[innerKey])
+                    }
+                    aux.push(
+                        {
+                            ...res.data[key],
+                            descricao: desc
+                        })
+                }
+
+                setProjetos(aux)
+                setIsLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+                setIsLoading(false);
+            })
+    }, []);
+
+    let portfolio;
+    if (!isLoading) {
+
+        portfolio = (
+            <>
+                {projetos.map(projeto => (
+
+                    <PortfolioCard key={projeto.url} title={projeto.titulo} url={projeto.url}
+                        src={projeto.preview} date={projeto.data}>
+
+                        {projeto.descricao.map(paragrafo => (
+                            <Typography key={paragrafo} variant="body1" component='p' gutterBottom
+                                dangerouslySetInnerHTML={{ __html: paragrafo }}
+                            />
+                        ))}
+
+                        <a className={classes.git} href={projeto.github}>
+                            <Button className={classes.git} variant='outlined'>
+                                <GitHub className={classes.icon} />
+                            </Button>
+                        </a>
+                    </PortfolioCard>
+                ))}
+            </>
+        );
+    }
 
     return (
         <div className={classes.root}>
@@ -68,102 +126,13 @@ const Portfolio = props => {
 
                 <Typography className={classes.title} variant='subtitle1' gutterBottom>
                     Portifólio
-                    </Typography>
+                </Typography>
             </div>
             <div className={classes.content}>
                 <div className={classes.text}>
+                    {isLoading ? <LinearProgress style={{width:'80vw'}}/> : null}
 
-
-                    <PortfolioCard title='GameDB' url='https://react-gamedb.firebaseapp.com/'
-                        src='https://firebasestorage.googleapis.com/v0/b/react-resume-samuel.appspot.com/o/gamedb.jpg?alt=media&token=a915ef53-7e7e-4511-942a-95d1137022ce'
-                        date='jan de 2020'>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Game DataBase, permite ao usuário pesquisar por jogos,
-                            ver seus detalhes e marca-los como favorito ou concluído.
-                            Também é possível visualizar a lista gerada pelo usuário em sua pagina de perfil.
-                        </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Os dados dos jogos são consumidos através de
-                            requisições HTTP a API <a href='https://rawg.io/apidocs'>rawg.</a>
-                        </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Aplicação desenvolvida com ReactJS e Material-UI utilizando bibliotecas como Redux, React-Redux, Axios e armazenando os dados no Firebase.
-                        </Typography>
-                        <a className={classes.git} href='https://github.com/samuel2412/react-gamedb'>
-                            <Button className={classes.git} variant='outlined'>
-                                <GitHub className={classes.icon} />
-                            </Button>
-                        </a>
-                    </PortfolioCard>
-
-                    <PortfolioCard title='Burger Builder' url='https://react-myburger-c854a.firebaseapp.com/'
-                        src='https://firebasestorage.googleapis.com/v0/b/react-resume-samuel.appspot.com/o/burgerBuilder.jpg?alt=media&token=4a2f825c-1446-4946-a9a5-f9f8e02716cc'
-                        date='dez de 2019 – jan de 2020'>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Aplicação que permite ao usuário selecionar os ingredientes que ele deseja em seu hambúrguer e realizar o pedido.
-                        </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Desenvolvida com ReactJS utilizando bibliotecas como Redux, React-Redux, Axios e armazenando os dados no Firebase.
-                        </Typography>
-                        <a className={classes.git} href='https://github.com/samuel2412/react-burger-app'>
-                            <Button className={classes.git} variant='outlined'>
-                                <GitHub className={classes.icon} />
-                            </Button>
-                        </a>
-                    </PortfolioCard>
-
-                    <PortfolioCard title='Pokedex' url='https://pokedex-react-samuel2412.netlify.com/'
-                        src='https://firebasestorage.googleapis.com/v0/b/react-resume-samuel.appspot.com/o/pokedex.jpg?alt=media&token=36cc16ba-4231-4ac7-aa9d-0bd41885071d'
-                        date='set de 2019 – dez de 2019'>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Aplicação desenvolvida com ReactJS que exibe uma lista de pokemons, permitindo ao usuário
-                            pesquisar e visualizar os detalhes do pokemon desejado.
-                            </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Os dados dos pokemons são consumidos através de
-                            requisições HTTP a API <a href='https://pokeapi.co/'>pokeapi.</a>
-                        </Typography>
-                        <a className={classes.git} href='https://github.com/samuel2412/pokedex-react'>
-                            <Button className={classes.git} variant='outlined'>
-                                <GitHub className={classes.icon} />
-                            </Button>
-                        </a>
-                    </PortfolioCard>
-
-
-                    <PortfolioCard title='Site de e-commerce' url='http://fierce-island-38673.herokuapp.com/'
-                        src='https://firebasestorage.googleapis.com/v0/b/react-resume-samuel.appspot.com/o/ecommerce.jpg?alt=media&token=fc56ffed-5a77-4fc2-a401-791e555552d7'
-                        date='set de 2019'>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Projeto construído usando SpringMVC, Hibernate, Java, JPA, JSP, Bootstrap, etc.
-                            </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Trata-se de uma aplicação que simula um e-commerce.
-                        </Typography>
-                        <a className={classes.git} href='https://github.com/samuel2412/ecommerce'>
-                            <Button className={classes.git} variant='outlined'>
-                                <GitHub className={classes.icon} />
-                            </Button>
-                        </a>
-                    </PortfolioCard>
-
-
-                    <PortfolioCard title='Financiamento de Experimentos' url='https://frozen-ridge-71151.herokuapp.com/'
-                        src='https://firebasestorage.googleapis.com/v0/b/react-resume-samuel.appspot.com/o/experimento.jpg?alt=media&token=9ab27caf-1be0-499b-b29b-17e3e835c4b7'
-                        date='set de 2019'>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Projeto construído usando SpringMVC, Hibernate, Java, JPA, JSP, Bootstrap, etc.
-                            </Typography>
-                        <Typography variant="body1" component='p' gutterBottom>
-                            Trata-se de uma aplicação que permite cadastros de Experimentos e que o usuário financie aqueles que o interesse.
-                        </Typography>
-                        <a className={classes.git} href='https://github.com/samuel2412/experimento'>
-                            <Button className={classes.git} variant='outlined'>
-                                <GitHub className={classes.icon} />
-                            </Button>
-                        </a>
-                    </PortfolioCard>
-
+                    {portfolio}
 
                 </div>
             </div>
